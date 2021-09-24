eb3129e17d62466e03eae50d2cd0829fd8ec45e3
Jkierem <juanmiguelgomez8@gmail.com>
2021-09-24 09:35:28 -0500

    Will finish some day

--- src/Maybe/index.ts
+++ src/Maybe/index.ts
@@ -1,10 +1,11 @@
 import type { Box, Nil } from "../_internals/types";
 import { Show } from "../union/show"
-import { isNil } from "../_internals/functions";
+import { isEmpty, isNil } from "../_internals/functions";
 import { Eq } from "../union/eq";
 import { getVariant } from "../_internals/symbols";
-import { Builder } from "../_internals/builder";
-import { Functor } from "../union/functor";
+import { alias, Builder } from "../_internals/builder";
+import { Foldable } from "../union/foldable";
+import { Thenable } from "../union/thenable";
 
 const maybeCases = ["Just" , "None"] as const
 
@@ -14,26 +15,52 @@ type BoxedMaybe<A> = Box<"Maybe",MaybeCases,A>
 
 export type Maybe<A> = BoxedMaybe<A> 
     & Show 
-    & Eq<"Maybe"> 
-    & { map: <B>(fn: (a: A) => B) => Maybe<B> }
-    & { chain: <B>(fn: (a: A) => Maybe<B>) => Maybe<B> }
+    & Eq<"Maybe">
+    & Foldable<undefined, A>
+    & Thenable<A>
+    & { 
+        fmap: <B>(fn: (a: A) => B) => Maybe<B>,
+        map: <B>(fn: (a: A) => B) => Maybe<B>,
+        
+        join: () => A,
+        flat: () => A,
+        chain: <B>(fn: (a: A) => Maybe<B>) => Maybe<B>,
+        bind: <B>(fn: (a: A) => Maybe<B>) => Maybe<B>,
+        flatMap: <B>(fn: (a: A) => Maybe<B>) => Maybe<B>,
+
+        tap: (fn: (a: A) => void) => Maybe<A>,
+        peak: (fn: (a: A) => void) => Maybe<A>,
+        effect: <B>(fn: (a: A) => Maybe<B>) => Maybe<A>,
+    }
 
 const maybeBuilder = Builder("Maybe", maybeCases)
 
 export const Just = <A>(a: A): Maybe<A> => {
     return maybeBuilder
         .forVariant("Just",a)
-        .mix(base => {
-            return {
-                ...base,
-                map: <B>(fn: (a: A) => B) => {
-                    return Just(fn(a))
-                },
-                chain: <B>(fn: (a: A) => Maybe<B>) => {
-                    return fn(a)
-                }
-            }
+        .mixWith({
+            fmap: <B>(fn: (a: A) => B) => {
+                return Just(fn(a))
+            },
+            chain<B>(fn: (a: A) => Maybe<B>){
+                return fn(a)
+            },
+            tap: (fn: (a: A) => void) => {
+                fn(a);
+                return Just(a) 
+            },
+            effect: <B>(fn: (a: A) => Maybe<B>) => {
+                return fn(a).map(() => a)
+            },
+            peak: alias("tap"),
+            map: alias("fmap"),
+            bind: alias("chain"),
+            flatMap: alias("chain"),
+            join: alias("get"),
+            flat: alias("get")
         })
+        .mix(Thenable())
+        .mix(Foldable())
         .mix(Eq())
         .mix(Show())
         .finish()
@@ -42,15 +69,33 @@ export const Just = <A>(a: A): Maybe<A> => {
 export const None = <A>(): Maybe<A> => {
     return maybeBuilder
         .forVariant("None", undefined as unknown as A)
+        .mixWith({
+            fmap<B>(_fn: (a: A) => B){ return this as unknown as Maybe<B> },
+            chain<B>(_fn: (a: A) => Maybe<B>){ return this as unknown as Maybe<B> },
+            tap: alias("fmap"),
+            peak: alias("tap"),
+            effect: alias("chain"),
+            map: alias("fmap"),
+            bind: alias("chain"),
+            flatMap: alias("chain"),
+            join: alias("get"),
+            flat: alias("get")
+        })
+        .mix(Thenable("Reject"))
+        .mix(Foldable("Left"))
         .mix(Eq((other) => getVariant(other) === "None"))
         .mix(Show(() => `[Maybe => None]`))
         .finish()
 }
 
+export const fromFalsy = <A>(a: A): Maybe<A> => a ? Just(a) : None<A>()
 export const fromNullish = <A>(a: A | Nil): Maybe<A> => isNil(a) ? None<A>() : Just(a)
+export const fromEmpty = <A>(a: A): Maybe<A> => isEmpty(a) ? Just(a) : None<A>()
+export const fromPredicate = <A>(pred: (a: A) => boolean, a: A): Maybe<A> => pred(a) ? Just(a) : None<A>()
+export const of = <A>(a: A): Maybe<A> => Just(a)
 
 export const Maybe = {
-    Just, None, fromNullish
+    Just, None, 
+    of, fromNullish, fromEmpty,
+    fromFalsy, fromPredicate
 }
-
-const f = Just(42)
